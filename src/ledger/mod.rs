e4a0ab7f3a59a9c30d622e5cca6a2bacc71bee2c
Kennet Postigo <kennetfpostigo@gmail.com>
2020-08-31 02:55:55 -0400

    Ledger, triggers, and logo update

--- src/ledger/mod.rs
+++ src/ledger/mod.rs
@@ -1,31 +1,42 @@
 use std::fmt::Debug;
 
+use crate::job::Status;
+
 pub struct Ledger {
-  alias: String,
-  ledger: Box<dyn History>,
+    pub alias: String,
+    pub ledger: Box<dyn History>,
 }
 
-pub trait History where Self: Send + Sync {
-  fn insert(&self, entry: (String, String)) -> Result<(), String>;
-  fn remove(&self, entry: (String, String)) -> Result<(), String>;
-  fn entry(&self, entry: (String, String)) -> Result<Option<String>, String>;
-  fn vclone(&self) -> Box<dyn History>;
+pub trait History
+where
+    Self: Send + Sync,
+{
+    fn insert(&self, store: &String, job: &String, status: &Status, time: &i64) -> Result<(), String>;
+    fn remove(&self, store: &String, job: &String, status: &Status, time: &i64) -> Result<(), String>;
+    fn entry(
+        &self,
+        store: &String,
+        job: &String,
+        status: &Status,
+        time: &i64,
+    ) -> Result<Option<(String, i64)>, String>;
+    fn vclone(&self) -> Box<dyn History>;
 }
 
 impl Clone for Ledger {
-  fn clone(&self) -> Self {
-      Ledger {
-          alias: self.alias.clone(),
-          ledger: self.ledger.vclone(),
-      }
-  }
+    fn clone(&self) -> Self {
+        Ledger {
+            alias: self.alias.clone(),
+            ledger: self.ledger.vclone(),
+        }
+    }
 }
 
 impl Debug for Ledger {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-      f.debug_struct("Ledger")
-          .field("alias", &self.alias)
-          .field("history", &"<history>")
-          .finish()
-  }
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Ledger")
+            .field("alias", &self.alias)
+            .field("history", &"<history>")
+            .finish()
+    }
 }
--- src/trigger/job_trigger.rs
+++ src/trigger/job_trigger.rs
@@ -1,25 +1,36 @@
-use async_trait::async_trait;
-
-use crate::trigger;
+use crate::job::Status;
 use crate::ledger::Ledger;
+use crate::trigger;
+use async_trait::async_trait;
 
 #[derive(Clone, Debug)]
 struct Trigger {
     job: String,
-    ledger: Ledger
+    store: String,
+    status: Status,
+    time: i64,
+    ledger: Ledger,
 }
 
 #[async_trait]
 impl trigger::Fire for Trigger {
     async fn should_run(&self) -> bool {
-      true
+        match self
+            .ledger
+            .ledger
+            .entry(&self.store, &self.job, &self.status, &self.time)
+        {
+            Ok(Some(_)) => true,
+            Ok(None) => false,
+            Err(_e) => false,
+        }
     }
 
     async fn next(&self) -> Option<u128> {
-        Some(1)
+        None
     }
 
     fn vclone(&self) -> Box<dyn trigger::Fire> {
-      Box::new(self.clone())
+        Box::new(self.clone())
     }
 }
