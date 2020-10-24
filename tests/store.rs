eb0ef0bf9ac9dc6422885fddeb4b0c13a6b853ef
Kennet Postigo <kennetfpostigo@gmail.com>
2020-10-24 11:14:38 -0400

    add remaining jobs for store of currently implemented methods on struct and trait

--- tests/store.rs
+++ tests/store.rs
@@ -60,3 +60,152 @@ fn store_add_job() {
     assert_equal!(store.jobs.len(), 1, "Store should have a job")
   });
 }
+
+#[test]
+fn store_remove_job() {
+  task::block_on(async {
+    let mut store = Store::new(String::from("exa"));
+
+    let start_time = {
+      let now = Utc::now().timestamp_nanos();
+      let delay: i64 = 10000000000;
+      let start_time = now + delay;
+      // println!("{}\n{}", now, now + delay);
+      start_time
+    };
+
+    let job = Job::new(String::from("job-1"), String::from("ls"), vec![]);
+
+    store
+      .add_job(
+        String::from("one"),
+        String::from("exec-one"),
+        start_time,
+        None,
+        Box::new(job),
+      )
+      .unwrap();
+
+    store.remove_job(&String::from("one")).unwrap();
+
+    assert_equal!(store.jobs.len(), 0, "Store should no job")
+  });
+}
+
+#[test]
+fn store_get_due_jobs() {
+  task::block_on(async {
+    let mut store = Store::new(String::from("exa"));
+
+    let start_time = {
+      let now = Utc::now().timestamp_nanos();
+      let delay: i64 = -500000000;
+      let start_time = now + delay;
+      // println!("{}\n{}", now, now + delay);
+      start_time
+    };
+
+    let job = Job::new(String::from("job-1"), String::from("ls"), vec![]);
+
+    store
+      .add_job(
+        String::from("one"),
+        String::from("exec-one"),
+        start_time,
+        None,
+        Box::new(job),
+      )
+      .unwrap();
+
+    assert_equal!(
+      store.get_due_jobs().unwrap().len(),
+      1,
+      "Store should have a job that is due"
+    )
+  });
+}
+
+#[test]
+fn store_get_due_jobs_2() {
+  task::block_on(async {
+    let mut store = Store::new(String::from("exa"));
+
+    let start_time = {
+      let now = Utc::now().timestamp_nanos();
+      let delay: i64 = 500000000;
+      let start_time = now + delay;
+      // println!("{}\n{}", now, now + delay);
+      start_time
+    };
+
+    let job = Job::new(String::from("job-1"), String::from("ls"), vec![]);
+
+    store
+      .add_job(
+        String::from("one"),
+        String::from("exec-one"),
+        start_time,
+        None,
+        Box::new(job),
+      )
+      .unwrap();
+
+    assert_equal!(
+      store.get_due_jobs().unwrap().len(),
+      0,
+      "Store should not have a job that is due"
+    )
+  });
+}
+
+#[test]
+fn store_get_due_jobs_3() {
+  task::block_on(async {
+    let mut store = Store::new(String::from("exa"));
+
+    let start_time = {
+      let now = Utc::now().timestamp_nanos();
+      let delay: i64 = -500000000;
+      let start_time = now + delay;
+      // println!("{}\n{}", now, now + delay);
+      start_time
+    };
+
+    let start_time_2 = {
+      let now = Utc::now().timestamp_nanos();
+      let delay: i64 = 500000000;
+      let start_time = now + delay;
+      // println!("{}\n{}", now, now + delay);
+      start_time
+    };
+
+    let job = Job::new(String::from("job-1"), String::from("ls"), vec![]);
+    let job2 = Job::new(String::from("job-2"), String::from("ls"), vec![]);
+
+    store
+      .add_job(
+        String::from("one"),
+        String::from("exec-one"),
+        start_time,
+        None,
+        Box::new(job),
+      )
+      .unwrap();
+
+    store
+      .add_job(
+        String::from("two"),
+        String::from("exec-two"),
+        start_time_2,
+        None,
+        Box::new(job2),
+      )
+      .unwrap();
+
+    assert_equal!(
+      store.get_due_jobs().unwrap().len(),
+      1,
+      "Store should have a job that is due"
+    )
+  });
+}
